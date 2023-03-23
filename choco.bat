71d295a13cf743bb3ce21519e3b953950d64db14
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-03-23 13:04:50 +0300

    :a: :b: :o2: :b: :a:

---
+++ choco.bat
@@ -0,0 +1,8 @@
+choco install 7zip
+choco install qbittorrent
+choco install googledrive
+choco install everything
+choco install notepadplusplus.install
+choco install ffmpeg
+choco install mkvtoolnix
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Hayward41/rdf/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
