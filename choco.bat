2d26a1daa0fe72113ce04a22d10161b948685f0b
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-03-23 13:24:54 +0300

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,3 +1,5 @@
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco feature enable -n=allowGlobalConfirmation
 choco install 7zip
 choco install qbittorrent
 choco install googledrive
