ed99b9641c83039b47cc1d6df852266f9392fbba
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-03-23 13:45:32 +0300

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,10 +1,11 @@
 @ECHO ON
 
-choco install 7zip
-choco install qbittorrent
-choco install googledrive
-choco install everything
-choco install notepadplusplus.install
-choco install ffmpeg
-choco install mkvtoolnix
+choco.exe install 7zip
+choco.exe install qbittorrent
+choco.exe install googledrive
+choco.exe install everything
+choco.exe install notepadplusplus.install
+choco.exe install ffmpeg
+choco.exe install mkvtoolnix
+
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Hayward41/rdf-1/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
