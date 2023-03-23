071c0bf06887bc1834a8a1deb01b2c55e4315a4a
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-03-23 14:07:04 +0300

    cleeanup&separators

--- choco.bat
+++ choco.bat
@@ -1,14 +1,20 @@
-@ECHO ON
-
+echo -------------------------------------------
 choco.exe install 7zip --no-progress
+echo -------------------------------------------
 choco.exe install qbittorrent --no-progress
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\qBittorrent\qBittorrent.lnk" "C:\Users\Public\Desktop\qbit.lnk"
+echo -------------------------------------------
 choco.exe install googledrive --no-progress
 copy "C:\Program Files\Google\Drive File Stream\launch.bat" "C:\Users\Public\Desktop\gdrive.lnk"
+echo -------------------------------------------
 choco.exe install everything --no-progress
+echo -------------------------------------------
 choco.exe install notepadplusplus.install --no-progress
+echo -------------------------------------------
 choco.exe install ffmpeg --no-progress
+echo -------------------------------------------
 choco.exe install mkvtoolnix --no-progress
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MKVToolNix\MKVToolNix GUI.lnk" "C:\Users\Public\Desktop\mkvtoolnix.lnk"
+echo -------------------------------------------
 
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Hayward41/rdf-1/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
