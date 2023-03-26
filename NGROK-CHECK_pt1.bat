6ecfcccf25087e9abf451c13605fb898220956cb
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-03-26 21:31:37 +0300

    Split ngrockcheck

--- NGROK-CHECK_pt1.bat
+++ NGROK-CHECK_pt1.bat
@@ -10,11 +10,3 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: *none*
 curl -O https://raw.githubusercontent.com/c9ffin/rdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
-diskperf -Y >nul
-sc start audiosrv >nul
-sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
-ping -n 10 127.0.0.1 >nul
