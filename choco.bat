80ab329f85b8fdabef133c0adce252e815c2b85c
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-04-04 16:52:02 +0300

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,7 +1,6 @@
 choco.exe install qbittorrent --no-progress
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\qBittorrent\qBittorrent.lnk" "C:\Users\Public\Desktop\qbit.lnk"
 choco.exe install 7zip --no-progress
-copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\qBittorrent\qBittorrent.lnk" "C:\Users\Public\Desktop\qbit.lnk"
 choco.exe install googledrive --no-progress
 copy "C:\Program Files\Google\Drive File Stream\launch.bat" "C:\Users\Public\Desktop\gdrive.bat"
 choco.exe install everything --no-progress
