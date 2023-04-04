c7fee344744dd6c16c7a2447877a8521167b1d1e
Hayward41 <82664161+Hayward41@users.noreply.github.com>
2023-04-04 16:50:57 +0300

    reorder

--- choco.bat
+++ choco.bat
@@ -1,5 +1,6 @@
-choco.exe install 7zip --no-progress
 choco.exe install qbittorrent --no-progress
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\qBittorrent\qBittorrent.lnk" "C:\Users\Public\Desktop\qbit.lnk"
+choco.exe install 7zip --no-progress
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\qBittorrent\qBittorrent.lnk" "C:\Users\Public\Desktop\qbit.lnk"
 choco.exe install googledrive --no-progress
 copy "C:\Program Files\Google\Drive File Stream\launch.bat" "C:\Users\Public\Desktop\gdrive.bat"
